c8462b24ae6146e2997a989ecf05403a533396f9
Zixi Luo <zixiluo@MacBook-Pro-2.local>
2018-08-09 09:16:12 +0800

    加了题目

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 #-*-coding:utf-8-*-
-from flask import Flask, g, flash, redirect, url_for
+from flask import Flask, g, flash, redirect, url_for,jsonify
 from flask import render_template
 from flask import request,session
 from server import login_server,register_server,score_server,recommend_server
 from server import score_detail,auth_server,all_user_s, pre_server,all_pre_s
 from server import zero_server
+from server import get_user_question,update_user_question
 import background
 
 app=Flask(__name__)
@@ -39,6 +40,28 @@ def score():
     else:
         flash('请先登陆')
         return redirect(url_for('login'))
+
+@app.route('/question/<user_id>',methods=['GET','POST'])
+def question(user_id):
+    if request.method=='POST':
+        try:
+            update_user_question(**request.form)
+        except Exception as e:
+            return e
+        return jsonify('seccess')
+
+    question_id,data,undone = get_user_question(user_id)
+    return jsonify({'questionId':question_id,
+                    'title':data[0],
+                    'options':[data[1],data[2],data[3],data[4]],
+                    'attributes':{
+                        'questionID':question_id,
+                        'answer':data[6],
+                        'answerExplain':data[5],
+                        },
+                    'totalQuestionNumber':undone,
+                    })
+
 
 @app.route('/recommend/',methods=['POST','GET'])
 def recommend():
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import sqlite3
 import re
 import datetime
+import random
 def login_server(username=None,password=None):
    conn = sqlite3.connect('test.sqlite')
    cursor = conn.cursor()
@@ -57,6 +58,40 @@ def score_server(user_id):
     data=cursor.fetchall()
     conn.close()
     return data[0]
+
+def update_user_question(**data):
+    conn = sqlite3.connect('test.sqlite')
+    cursor = conn.cursor()
+    cursor.execute('insert into user_questions (user_id,question_id,result,\
+                choice) values(?,?,?,?)',\
+                    (data['userId'][0],data['questionId'][0],\
+                    data['answerResult'][0],data['userChoice'][0]))
+    conn.commit()
+    conn.close()
+
+def get_user_question(user_id):
+    conn = sqlite3.connect('test.sqlite')
+    cursor = conn.cursor()
+    cursor.execute('select question_id from user_questions where user_id=?',(user_id,))
+    done=[x[0] for x in cursor.fetchall()]
+    cursor.execute('select id from questions')
+    questions= [x[0] for x in cursor.fetchall()]
+    conn.close()
+    undone=[]
+    for q in questions:
+        if q not in done:
+            undone.append(q)
+    r=random.choice(undone)
+    return [r,get_question(r),len(undone)-1]
+
+
+def get_question(question_id):
+    conn = sqlite3.connect('test.sqlite')
+    cursor = conn.cursor()
+    cursor.execute('select title,A,B,C,D,explain,answer from questions where id=?',(question_id,))
+    data,=cursor.fetchall()
+    conn.close()
+    return data
 
 def score_detail(user_id):
     conn = sqlite3.connect('test.sqlite')
